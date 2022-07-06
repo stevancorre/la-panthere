005f0a79220852de01b98bd8bd1a60601e797e09
Stévan Corre <stevancorre@protonmail.com>
2022-07-06 15:55:21 +0200

    [gruntfile] fix target names

--- gruntfile.js
+++ gruntfile.js
@@ -2,21 +2,21 @@ module.exports = function (grunt) {
     grunt.initConfig({
         clean: ["dist/*"],
         copy: {
-            main: {
+            dist: {
                 files: [
                     { expand: true, cwd: "public", src: ["**"], dest: "dist" },
                 ],
             },
         },
         uglify: {
-            my_target: {
+            dist: {
                 files: {
                     "dist/index.min.js": ["src/js/jquery-2.1.0.js", "src/js/*.js"],
                 }
             }
         },
         cssmin: {
-            build: {
+            dist: {
                 src: "src/css/*.css",
                 dest: "dist/style.min.css"
             }
@@ -36,7 +36,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-contrib-cssmin");
-    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-uncss");
 
     grunt.registerTask("default", ["clean", "copy", "uglify", "cssmin", "uncss"]);
